6975d8d1133bafff25984001fac7cd2b188aef6e
KhaledSami <k.sami.mohammed@gmail.com>
2015-01-01 17:03:48 +0100

    Modify parse function to accept defaults and boolean parameters

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -10,5 +10,27 @@ describe('Tags', function() {
             expect(results).to.have.a.property('depth', 4);
             expect(results).to.have.a.property('hello', 'world');
         });
+        it('should fallback to defaults', function() {
+            var args = ['--depth=4', '--hello=world'];
+            var defaults = {
+                depth: 2,
+                foo: 'bar'
+            };
+            var results = tags.parse(args, defaults);
+
+            var expected = {
+                depth: 4,
+                foo: 'bar',
+                hello: 'world'
+            };
+
+            expect(results).to.deep.equal(expected);
+        });
+        it('should accept tags without values as a bool', function() {
+            var args = ['--searchContents'];
+            var results = tags.parse(args);
+
+            expect(results).to.have.a.property('searchContents', true);
+        });
     });
 });
